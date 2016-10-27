89e8ac700dda206fa8e614496668dd005db0964a
WiiPlayer2 <waldetomme@gmail.com>
2016-10-27 22:31:37 +0200

    Added Shell and ShellManager

---
+++ Sakuya.Core/Shell.cs
@@ -0,0 +1,38 @@
+﻿using SharpLua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakuya.Core
+{
+    public class Shell<T> : IDisposable
+    {
+        public Shell(T id)
+        {
+            ID = id;
+
+            Lua = new LuaInterface();
+
+            RemoveVars("new", "SharpLua", "Lua", "System", "clr", "luanet");
+        }
+
+        public LuaInterface Lua { get; private set; }
+
+        public T ID { get; protected set; }
+
+        private void RemoveVars(params string[] names)
+        {
+            foreach (var n in names)
+            {
+                Lua.DoString(string.Format("{0} = nil", n));
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sakuya.Core/ShellManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakuya.Core
+{
+    public abstract class ShellManager<T, S>
+        where S : Shell<T>
+    {
+        public delegate void ShellOpenedEventHandler(object sender, Shell<T> shell);
+        public delegate void ShellClosedEventHandler(object sender, T id);
+
+        private Dictionary<T, S> shells;
+
+        public ShellManager()
+        {
+            shells = new Dictionary<T, S>();
+        }
+
+        public S this[T id]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public S OpenShell(T id)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract S CreateShell(T id);
+
+        public void CloseShell(T id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event ShellOpenedEventHandler ShellOpened = (_, __) => { };
+        public event ShellClosedEventHandler ShellClosed = (_, __) => { };
+    }
+}
